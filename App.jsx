2ba6bd958ffc5c5f71082b94135040e6cb4f0891
Sarthakadhav <adhavsarthak0848@gmail.com>
2025-07-02 12:10:35 +0530

    App

--- App.jsx
+++ App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Home Screen</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
