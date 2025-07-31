e8e7c647d5e9a9ca0e50cc444e019215b7111625
sanskruti bhavsar <sanskrutiab88@gmail.com>
2025-07-31 22:02:14 +0530

    blood pressure card completed

--- App.jsx
+++ App.jsx
@@ -4,13 +4,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MeasureScreen from './Src/Screen/MeasureScreen';
 import { View } from 'react-native';
+import Bloodpress from "./Src/Screen/Bloodpress"
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
 <View style={{flex:1}}>
-  <MeasureScreen/>
+  <Bloodpress/>
 </View>
   );
 }
