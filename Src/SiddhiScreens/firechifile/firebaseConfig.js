169006ad1a62623990fae9c49ecdbb2664c5757d
Bothe Siddhi <bothesiddhi12@gmail.com>
2025-08-09 23:37:38 +0530

    sid recent done

---
+++ Src/SiddhiScreens/firechifile/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from '@react-native-firebase/app';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKwPZstPGtVuL7qbWKK07LW1KDLAqDQgA",
+  authDomain: "internpro-e969e.firebaseapp.com",
+  projectId: "internpro-e969e",
+  storageBucket: "internpro-e969e.appspot.com", 
+  messagingSenderId: "678464281008",
+  appId: "1:678464281008:web:97cee81e6d2bd54cb39e75",
+  measurementId: "G-HY93SK54FN"
+};
+
+export { firebase, auth, firestore }
--- src/SiddhiScreens/OtpVerification.jsx
+++ src/SiddhiScreens/OtpVerification.jsx
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useRoute, useNavigation } from '@react-navigation/native';
-import { firestore } from '../firechifile/firebaseConfig';
+import { firestore } from './firechifile/firebaseConfig';
 
 const OtpVerification = () => {
   const route = useRoute();
