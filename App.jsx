ac7ba70a7ba30d23c88d0ca390b4f9082344c076
Bothe Siddhi <bothesiddhi12@gmail.com>
2025-08-11 06:44:58 +0530

    Revert "Update App.jsx added bottom tab"
    This reverts commit c3f081408ea34aeb6f900109308ac45b4422ceaa.

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React,{ useEffect } from 'react';
 
-// Screens
-import HomeScreen from './src/Screen/HomeScreen';
-import MedicationCard from './src/Screen/MedicationCard';
-import Setschedulepg from './src/Screen/Setschedulepg';
+import HomeScreen from './src/Screen/HomeScreen'
+import MedicationCard from './src/Screen/MedicationCard'
+import Setschedulepg from './src/Screen/Setschedulepg'
 import MultiplePolicy from './src/Screens/MultiplePolicy';
 import InsuranceSrc1 from './src/Screens/InsuranceSrc1';
 import InsurancePreview from './src/Screens/InsurancePreview';
@@ -15,26 +14,26 @@ import MedicalInfo from './src/Screens/MedicalInfo';
 import Maintab from './src/Screen/Maintab.jsx';
 
 // sarthak
-import DoctorSuggestionScreen from './src/Screen/DoctorSuggestionScreen';
-import EmergencyContactScreen from './src/Screen/EmergencyContactScreen';
-import QRScreen from './src/Screen/QRScreen';
-import ProfileScreen from './src/Screen/ProfileScreen';
-import NotificationScreen from './src/Screen/NotificationScreen';
-import ChangePasswordScreen from './src/Screen/ChangePasswordScreen';
-
-// siddhi
+import DoctorSuggestionScreen from './src/Screen/DoctorSuggestionScreen'
+import EmergencyContactScreen from './src/Screen/EmergencyContactScreen'
+import QRScreen from './src/Screen/QRScreen'
+import ProfileScreen from './src/Screen/ProfileScreen'
+import NotificationScreen from './src/Screen/NotificationScreen'
+import ChangePasswordScreen from './src/Screen/ChangePasswordScreen'
+// siiddhi
 import ForgotPassword from './src/SiddhiScreens/ForgotPassword.jsx';
 import Onboard from './src/SiddhiScreens/Onboard.jsx';
 import Signup from './src/SiddhiScreens/Signup.jsx';
 import Login from './src/SiddhiScreens/Login.jsx';
 import OtpVerification from './src/SiddhiScreens/OtpVerification.jsx';
 
-// palak
+//palak
 import StartInsuranceFile from './src/PalakScreens/StartInsuranceFile';
 import MedicalReportPreview from './src/PalakScreens/MedicalReportPreview.jsx';
 import Terms from './src/PalakScreens/Terms.jsx';
 import TermsSrc2 from './src/PalakScreens/TermsSrc2.jsx';
 
+import { db } from "./src/firebaseConfig"; // adjust path as needed
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -42,44 +41,58 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+    useEffect(() => {
+    async function setupNotifications() {
+      await notifee.requestPermission();
+      await notifee.createChannel({
+        id: 'reminder',
+        name: 'Reminder Notifications',
+        importance: AndroidImportance.HIGH,
+      });
+    }
+
+    setupNotifications();
+  }, []);
+ 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Onboard" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="MedicationCard" component={MedicationCard} options={{ headerShown: false }} />
+        <Stack.Screen name="Setschedulepg" component={Setschedulepg} options={{ headerShown: false }} />
+        <Stack.Screen name="MultiplePolicy" component={MultiplePolicy} options={{ headerShown: false }} />
+        <Stack.Screen name="PersonalDetails" component={PersonalDetails} options={{ headerShown: false }} />
+        <Stack.Screen name="EmergencyContact" component={EmergencyContact} options={{ headerShown: false }} />
+        <Stack.Screen name="InsuranceSrc1" component={InsuranceSrc1} options={{ headerShown: false }} />
+        <Stack.Screen name="InsurancePreview" component={InsurancePreview} options={{ headerShown: false }} />
+        <Stack.Screen name="MedicalInfo" component={MedicalInfo} options={{ headerShown: false }} />
+        <Stack.Screen name="Maintab" component={Maintab} options={{headerShown:false}}/>      
+        {/* // PalakScreens */}
+         <Stack.Screen name="MedicalReportPreview" component={MedicalReportPreview} options={{ headerShown: false }} />
+         <Stack.Screen name="Terms" component={Terms} options={{ headerShown: false }} />
+         <Stack.Screen name="TermsSrc2" component={TermsSrc2} options={{ headerShown: false }} />
+         <Stack.Screen name="StartInsuranceFile" component={StartInsuranceFile} options={{ headerShown: false }} />
 
-        {/* Flow before showing bottom tab */}
-        <Stack.Screen name="Onboard" component={Onboard} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="OtpVerification" component={OtpVerification} />
-        <Stack.Screen name="Terms" component={Terms} />
-        <Stack.Screen name="PersonalDetails" component={PersonalDetails} />
-
-        {/* Main app with bottom tab */}
-        <Stack.Screen name="Maintab" component={Maintab} />
-
-        {/* Other screens accessible from Maintab */}
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="MedicationCard" component={MedicationCard} />
-        <Stack.Screen name="Setschedulepg" component={Setschedulepg} />
-        <Stack.Screen name="MultiplePolicy" component={MultiplePolicy} />
-        <Stack.Screen name="EmergencyContact" component={EmergencyContact} />
-        <Stack.Screen name="InsuranceSrc1" component={InsuranceSrc1} />
-        <Stack.Screen name="InsurancePreview" component={InsurancePreview} />
-        <Stack.Screen name="MedicalInfo" component={MedicalInfo} />
-        <Stack.Screen name="MedicalReportPreview" component={MedicalReportPreview} />
-        <Stack.Screen name="TermsSrc2" component={TermsSrc2} />
-        <Stack.Screen name="StartInsuranceFile" component={StartInsuranceFile} />
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-        <Stack.Screen name="DoctorSuggestionScreen" component={DoctorSuggestionScreen} />
-        <Stack.Screen name="EmergencyContactScreen" component={EmergencyContactScreen} />
-        <Stack.Screen name="QRScreen" component={QRScreen} />
-        <Stack.Screen name="NotificationScreen" component={NotificationScreen} />
-        <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} />
-
+{/* sarthak */}
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }} />
+         <Stack.Screen name="DoctorSuggestionScreen" component={DoctorSuggestionScreen} options={{ headerShown: false }} />
+         <Stack.Screen name="EmergencyContactScreen" component={EmergencyContactScreen} options={{ headerShown: false }} />
+         <Stack.Screen name="QRScreen" component={QRScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="NotificationScreen" component={NotificationScreen} options={{ headerShown: false }}/>
+          <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} options={{ headerShown: false }} />
+      {/* siidhi */}
+         <Stack.Screen name="Onboard" component={Onboard} options={{ headerShown: false }}/>
+        <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }}/>
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }}/>
+        <Stack.Screen name="OtpVerification" component={OtpVerification} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
-export default App;
+export default App
+
+
+
+
