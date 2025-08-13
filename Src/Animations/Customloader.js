ad7afbd6f647502805992c0408d15f52f6f4bd9f
Bothe Siddhi <bothesiddhi12@gmail.com>
2025-08-13 15:34:15 +0530

    web pg ui done

--- Src/Animations/Customloader.js
+++ Src/Animations/Customloader.js
@@ -5,24 +5,29 @@ import LottieView from 'lottie-react-native';
 const HeartLoader = () => {
   return (
     <View style={styles.container}>
-   <LottieView
-  source={require('./Loader.json')}
-  autoPlay
-  loop
-  style={{ width: 270, height: 270 }}
-/>
-
+      <LottieView
+        source={require('./Loader.json')}
+        autoPlay
+        loop
+        style={{ width: 270, height: 270 }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
-   
-
+    backgroundColor: '#fff',
+    zIndex: 9999,
   },
 });
 
